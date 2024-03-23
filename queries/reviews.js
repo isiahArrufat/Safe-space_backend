174e6e3a6cbe8c74c878b525b06447a807b04077
brenda-soto <bsoto@pursuit.org>
2024-03-23 15:47:16 -0400

    Fixed PUT bug for reviews

--- queries/reviews.js
+++ queries/reviews.js
@@ -1,77 +1,85 @@
 const teapots = require("../controllers/teapotsController.js");
-const db = require("../db/dbConfig.js")
-
+const db = require("../db/dbConfig.js");
 
 //Index
 const getAllReviews = async (teapot_id) => {
-    try {
-        const allReviews = await db.any(`SELECT * FROM reviews WHERE teapot_id=$1`, teapot_id);
-        return allReviews
-    } catch (error) {
-        return error;
-    }
+  try {
+    const allReviews = await db.any(
+      `SELECT * FROM reviews WHERE teapot_id=$1`,
+      teapot_id
+    );
+    return allReviews;
+  } catch (error) {
+    return error;
+  }
 };
 
 //Show
 const getOneReview = async (id) => {
-    try {
-        const oneReview = await db.one(`SELECT * FROM reviews WHERE id=$1`, id)
-        return oneReview
-    } catch (error) {
-        return error
-    }
-}
+  try {
+    const oneReview = await db.one(`SELECT * FROM reviews WHERE id=$1`, id);
+    return oneReview;
+  } catch (error) {
+    return error;
+  }
+};
 
 //Create
 const createReview = async (review) => {
-    try {
-      const newReview = await db.one(
-        "INSERT INTO reviews (content, rating, teapot_id, user_id, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
-        [
-          review.content,
-          review.rating,
-          review.teapot_id,
-          review.user_id,
-          review.created_at,
-          review.updated_at
-        ]
-      );
-      return newReview;
-    } catch (error) {
-      return error;
-    }
-  };
-  
+  try {
+    const newReview = await db.one(
+      "INSERT INTO reviews (content, rating, teapot_id, user_id, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+      [
+        review.content,
+        review.rating,
+        review.teapot_id,
+        review.user_id,
+        review.created_at,
+        review.updated_at,
+      ]
+    );
+    return newReview;
+  } catch (error) {
+    return error;
+  }
+};
 
 //Delete
 const deleteReview = async (id) => {
   try {
     const deletedReview = await db.one(
-      "DELETE FROM reviews WHERE id = $1 RETURNING *", id
-    )
-    return deletedReview
+      "DELETE FROM reviews WHERE id = $1 RETURNING *",
+      id
+    );
+    return deletedReview;
   } catch (error) {
     return error;
   }
-}
-
+};
 
 //Update
 const updateReview = async (review) => {
   try {
     const updatedReview = await db.one(
-      "UPDATE reviews SET content=$1, rating=$2, updated_at=$3 where id = $4 RETURNING *", [
+      "UPDATE reviews SET content=$1, rating=$2, updated_at=$3, teapot_id=$4 WHERE id=$5 RETURNING *",
+      [
         review.content,
         review.rating,
         review.updated_at,
-        review.id
+        review.teapot_id,
+        review.review_id,
       ]
-    )
-    return updatedReview
+    );
+    return updatedReview;
   } catch (error) {
-    return error
+    return error;
   }
-}
+};
 
-
-module.exports = {getAllReviews, getOneReview, createReview, deleteReview, updateReview}
+module.exports = {
+  getAllReviews,
+  getOneReview,
+  createReview,
+  deleteReview,
+  updateReview,
+};
