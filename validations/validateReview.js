118473cb0d1a825b066a3c74d2d035c00fcdc4ad
AnitaOwen <anitaowen@pursuit.org>
2024-03-28 10:22:22 -0400

    added validation for post and put reviews

--- validations/validateReview.js
+++ validations/validateReview.js
@@ -1,5 +1,5 @@
 function checkContentRating(req, res, next) {
-    const { rating, content } = req.body
+    const { rating, content, user_id, teapot_id } = req.body
 
     // check if rating exists is between 1 and 5
     if(!rating || rating < 1 || rating > 5){
@@ -9,6 +9,16 @@ function checkContentRating(req, res, next) {
     // check if content exists and is a string
     if(!content || typeof content !== 'string') {
         return res.status(400).json({ error: 'Please enter your review.'})
+    }
+
+    // check if user_id exists and is a number
+    if(!user_id || typeof user_id !== 'number') {
+        return res.status(400).json({ error: 'User id is missing.'})
+    }
+
+    // check if teapot_id exists and is a number
+    if(!teapot_id || typeof teapot_id !== 'number') {
+        return res.status(400).json({ error: 'Teapot id is missing.'})
     }
     next()
 }
