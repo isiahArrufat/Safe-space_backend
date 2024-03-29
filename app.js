8e5a6c975694dacd45fc42bd3cdb9f9d14cbd491
jdrichards-pursuit <jdrpursuit@gmail.com>
2024-03-29 17:32:54 -0400

    change cors origin

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
 // DEPENDENCIES
-const cors = require('cors')
-const express = require('express')
-const csrf = require('csurf')
-const cookieParser = require('cookie-parser')
-const userController = require('./controllers/userController')
-const authController = require('./controllers/authController')
-const teapotsController = require('./controllers/teapotsController')
+const cors = require("cors");
+const express = require("express");
+const csrf = require("csurf");
+const cookieParser = require("cookie-parser");
+const userController = require("./controllers/userController");
+const authController = require("./controllers/authController");
+const teapotsController = require("./controllers/teapotsController");
 
 // CONFIGURATION
-const app = express()
+const app = express();
 
 // MIDDLEWARE
 app.use(
   cors({
-    origin: 'https://main--teawhips.netlify.app', // Allow only this origin to access
+    origin: "https://main--exquisite-pudding-29dd47.netlify.app", // Allow only this origin to access
     credentials: true, // Allow cookies and headers to be sent along with the request
   })
-)
-app.use(express.json())
-app.use(cookieParser())
-const csrfProtection = csrf({ cookie: true })
+);
+app.use(express.json());
+app.use(cookieParser());
+const csrfProtection = csrf({ cookie: true });
 
-app.use(csrfProtection)
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
-  res.cookie('XSRF-TOKEN', req.csrfToken())
-  next()
-})
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  next();
+});
 
-app.use('/api/users', userController)
-app.use('/api/auth', authController)
-app.use('/api/teapots', teapotsController)
+app.use("/api/users", userController);
+app.use("/api/auth", authController);
+app.use("/api/teapots", teapotsController);
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to TeaWhips!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to TeaWhips!");
+});
 
 // 404 PAGE
-app.get('*', (req, res) => {
-  res.status(404).send('Page not found')
-})
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
 
 // EXPORT
-module.exports = app
+module.exports = app;
