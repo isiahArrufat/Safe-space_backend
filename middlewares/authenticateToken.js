57537ab13e6b4f3adc32904b7f92036f292793ce
jdrichards-pursuit <jdrpursuit@gmail.com>
2024-03-30 12:17:09 -0400

    log token

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -2,9 +2,9 @@ const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-
+  console.log("Authorization Header:", authHeader);
   const token = authHeader && authHeader.split(" ")[1];
-
+  console.log("authenticateToken:", token);
   if (token == null) {
     return res.sendStatus(401).json({ message: "Unauthorized" });
   }
