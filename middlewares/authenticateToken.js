b23140cf5553f5185222d475c51b2e1c2f666625
jdrichards-pursuit <jdrpursuit@gmail.com>
2024-04-01 19:10:30 -0400

    add user to message

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -4,7 +4,7 @@ const authenticateToken = async (req, res, next) => {
   const authHeader = await req.headers["authorization"];
 
   if (authHeader.includes("null"))
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ message: "Unauthorized User" });
   else {
     const token = authHeader && (await authHeader.split(" ")[1]);
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
