c86c6904db9da4e3890e36a05e35a240a1145624
jdrichards-pursuit <jdrpursuit@gmail.com>
2024-04-05 09:50:17 -0400

    return local

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,16 +2,16 @@ const pgp = require("pg-promise")();
 require("dotenv").config();
 
 // for local db
-// const cn = {
-//   host: process.env.PG_HOST,
-//   port: process.env.PG_PORT,
-//   database: process.env.PG_DATABASE,
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PASSWORD,
-// };
+const cn = {
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+};
 
 // for deployment using ElephantSQL comment the cn variable above out and comment the cn variable below in
-const cn = process.env.CONNECTION_STRING;
+// const cn = process.env.CONNECTION_STRING;
 
 const db = pgp(cn);
 
