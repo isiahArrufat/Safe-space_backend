857742c9186f68fec82b7a3f89aca1fb26e7a4d6
Isiah Arrufat <isiaharrufat@pursuit.org>
2024-04-13 09:29:26 -0400

    created all routes for entries resource

--- controllers/entriesController.js
+++ controllers/entriesController.js
@@ -21,7 +21,7 @@ entries.get('/', async (req, res) => {
         res.status(500).json({ error: 'server error' });
     }
 });
-
+// show
 entries.get('/:id', async (req, res) =>{
     try {
         const { id } = req.params;
@@ -30,6 +30,39 @@ entries.get('/:id', async (req, res) =>{
             res.json(entry);
         } else {
             res.status(404).json({ error: 'entry not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'server error' });
+    }
+});
+// create
+entries.post('/', async (req, res) => {
+    try {
+     const entry = await newEntry(req.body);
+     res.json(entry);
+    } catch (error) {
+         res.status(400).json({ error: "unable to create entry"});
+     }
+ });
+// update
+ entries.put("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatedEntry = await updateEntry(id, req.body);
+        res.status(200).json(updatedEntry);
+    } catch (error) {
+        res.status(500).json({ error: 'server error' });
+    }
+});
+// delete
+entries.delete("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedEntry = await deleteEntry(id);
+        if (deletedEntry.id) {
+            res.status(200).json(deletedEntry);
+        } else {
+            res.status(404).json("song not found");
         }
     } catch (error) {
         res.status(500).json({ error: 'server error' });
